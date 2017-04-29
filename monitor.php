7c02e95d7d86915fdc6d2640faa5c72d243e8d44
Omar Malave <omalave@gmail.com>
2017-04-29 15:59:55 -0500

    change miner condition

--- monitor.php
+++ monitor.php
@@ -1,15 +1,11 @@
 <?php
 
-$urlMiner = "http://xxxxxx.ethosdistro.com/?json=yes";
+$urlMiner = "http://736972.ethosdistro.com/?json=yes";
 
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
 curl_setopt($ch, CURLOPT_URL,$urlMiner);
-
 $result=curl_exec($ch);
 
 curl_close($ch);
@@ -17,47 +13,33 @@ curl_close($ch);
 $dataRigs = json_decode($result, true);
 foreach ($dataRigs["rigs"] as $worker) {
 
-	if ($worker["condition"] == "mining") {
+	if ($worker["condition"] == "stuck_miners") {
 
-		$workerGpuHashes = $worker["miner_hashes"];
+		require_once "Mail-1.4.1/Mail.php";
+		$worker  = key($dataRigs["rigs"]);
+		$from    = '<xxxxxx@xxxxxx.com>';
+		$to      = '<xxxxxx@xxxxxx.com>';
+		$subject = 'Alerta: Worker '.$worker;
+		$body    = "Tenemos un problema con el Worker ".$worker.", un GPU esta apagado...";
 
-		$wrHash = explode(" ", $workerGpuHashes);
-		$alert  = 0;
+		$headers = array(
+			'From'    => $from,
+			'To'      => $to,
+			'Subject' => $subject
+		);
 
-		foreach ($wrHash as $key => $value) {
+		$smtp = Mail::factory('smtp', array(
+		        'host' => 'ssl://smtp.gmail.com',
+		        'port' => '465',
+		        'auth' => true,
+		        'username' => 'xxxxxx',
+		        'password' => 'xxxxxx'
+		    ));
 
-			if ($value == 0.00) {
-				$alert = 1;
-			}
-		}
+		$mail = $smtp->send($to, $headers, $body);
 
-		if ($alert == 1) {
-
-			require_once "Mail-1.4.1/Mail.php";
-			$worker  = key($dataRigs["rigs"]);
-			$from    = '<xxxxxx@xxxxxx.com>';
-			$to      = '<xxxxxx@xxxxxx.com>';
-			$subject = 'Alerta: Worker '.key($dataRigs["rigs"]);
-			$body    = "Tenemos un problema con el Worker ".$worker.", un GPU esta apagado...";
-
-			$headers = array(
-				'From'    => $from,
-				'To'      => $to,
-				'Subject' => $subject
-			);
-
-			$smtp = Mail::factory('smtp', array(
-			        'host' => 'ssl://smtp.gmail.com',
-			        'port' => '465',
-			        'auth' => true,
-			        'username' => '',
-			        'password' => ''
-			    ));
-
-			$mail = $smtp->send($to, $headers, $body);
-
-		}
 	}
+
 }
 
 ?>
